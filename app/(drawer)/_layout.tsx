2ebc95797a0240885ac2a3fda1bf16d160af1af5
Evan Kim <evanhkim09@gmail.com>
2025-09-23 23:20:23 -0400

    More fixes

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -42,8 +42,8 @@ export default function DrawerLayout() {
             <Drawer.Screen
                 name="findProducts"
                 options={{
-                    drawerLabel: 'Find Acne Products',
-                    title: 'Find Acne Produtcs',
+                    drawerLabel: 'AI-Recommended Products',
+                    title: 'AI-Recommended Products',
                 }}
             />
         </Drawer>
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,34 +3,36 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = "#0a7ea4";
+const tintColorDark = "#fff";
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: "#11181C",
+    background: "#fff",
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
+    darkerTint: "#171717",
+    icon: "#687076",
+    tabIconDefault: "#687076",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
+    text: "#ECEDEE",
+    background: "#151718",
+    tint: "#404040",
+    darkerTint: "#262626",
+    icon: "#9BA1A6",
+    tabIconDefault: "#9BA1A6",
     tabIconSelected: tintColorDark,
   },
-  error: '#fb2c36',
-  primary_100: '#F3EEFB',
-  primary_200: '#E6DDF8',
-  primary_300: '#DFD1FA',
-  primary_400: '#D6C5F7',
-  primary_500: '#C7B3EF',
-  primary_600: '#B8A1E3',
-  primary_700: '#b099dbff',
-  primary_800: '#a288d3ff',
-  primary_900: '#8E6EC9'
+  error: "#fb2c36",
+  primary_100: "#F3EEFB",
+  primary_200: "#E6DDF8",
+  primary_300: "#DFD1FA",
+  primary_400: "#D6C5F7",
+  primary_500: "#C7B3EF",
+  primary_600: "#B8A1E3",
+  primary_700: "#b099dbff",
+  primary_800: "#a288d3ff",
+  primary_900: "#8E6EC9",
 };
